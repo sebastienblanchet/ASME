7df564d72fa2ec8f91b63511bbc73737de8c0d1a
sebastienblanchet <seb.blanchet.007@gmail.com>
2017-04-06 14:19:10 -0400

    Export itsum to csv, added logging

--- Div_II.py
+++ Div_II.py
@@ -2,14 +2,24 @@
 # Note: all calculations to be completed in US customary
 
 # Import relevant modules
+import sys
+from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import genfromtxt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+# Logging
+nowtext = datetime.now().strftime('%Y_%m_%d_%H%M%S')
+sys.stdout = open('log/' + nowtext + '.txt', 'w')
+print(datetime.now().strftime('%Y/%m/%d %H:%M:%S'))
+print()
 
 # Define all functions
-
-# bilinear interpolation equation set y_2 = array
-def getA(mtrx, arr_dt, dt, arr_ld, ld):
+# Use bilinear interpolation equation get A array
+def getA( mtrx, arr_dt, dt, arr_ld, ld):
 
     x2 = (np.abs(arr_dt - dt)).argmin()
     y2 = (np.abs(arr_ld - ld)).argmin()
@@ -21,11 +31,13 @@ def getA(mtrx, arr_dt, dt, arr_ld, ld):
     if ld >= arr_ld[y2]:
         y2 += 1
 
-    print('ACTUAL:      D/t = %.1f and L/D = %3f' % (dt, ld))
-    print('UPPER BOUND: D/t = %.1f and L/D = %3f' % (arr_dt[x2], arr_ld[y2]))
-
     x1 = x2 - 1
     y1 = y2 - 1
+
+    # Print status for debug
+    print('TARGET:      D/t = %.1f      L/D = %3f' % (dt, ld))
+    print('UPPER BOUND: D/t = %.1f      L/D = %3f' % (arr_dt[x2], arr_ld[y2]))
+    print('LOWER BOUND: D/t = %.1f      L/D = %3f' % (arr_dt[x1], arr_ld[y1]))
 
     a_11 = mtrx[y1, x1]
     a_21 = mtrx[y1, x2]
@@ -38,11 +50,9 @@ def getA(mtrx, arr_dt, dt, arr_ld, ld):
     fx_y1 = ((x2-x)/(x2-x1))*a_11 + ((x-x1)/(x2-x1))*a_21
     fx_y2 = ((x2-x)/(x2-x1))*a_12 + ((x-x1)/(x2-x1))*a_22
 
-
+    print()
     print('A21 = %.4f   A22 = %.4f' % (a_12, a_22))
     print('A11 = %.4f   A21 = %.4f' % (a_11, a_21))
-
-    # if a_11 & a_21 & a
 
     a = ((y2-y)/(y2-y1))*fx_y1 + ((y-y1)/(y2-y1))*fx_y2
 
@@ -72,7 +82,7 @@ def getB(arr, val):
     return b
 
 # Define all parameters
-D_o = 28                      # diameter
+D_o = 28                    # diameter
 d_tether = 15.2/25.4        # tether diameter 15.2mm to [in]
 L_tether = (370*1000)/25.4  # tether length overall 370m to [in]
 P = 224.809*51.264          # force P of 51.264 kN [lbs]
@@ -82,7 +92,10 @@ E_y = 2.9*10**7             # Youngs modulus 200 [GPa]
 v = 0.3                     # poissons ratio [ul]
 rho = 0.284                 # density of steel [lb/in^3]
 SG = 1.01                   # spool gap 1% [ul]
-t_0 = 1/2                  # initial thickness guess
+t_0 = 1/2                   # initial thickness guess
+t_step = 1/8                # step for convergence
+maxit = 5                  # max iterations avoid infinite loop
+
 
 # Calculated initial parameters
 S_allow = S_y/SF
@@ -111,6 +124,7 @@ rows = FigG_LD.__len__()
 # Create solution matrix
 FigG_A3D = np.zeros((rows, cols))
 
+# loop for processing csv of A data
 for i in range(rows):
     for j in range(cols):
 
@@ -125,8 +139,15 @@ for i in range(rows):
 p_a = 0
 t = t_0
 itnum = 1
-maxit = 25
-t_step = 1/8
+
+# Initliaze arrays for summary csv
+it = []
+t_arr = []
+A_arr = []
+B_arr = []
+Dt_arr = []
+LD_arr = []
+pa_arr = []
 
 # Create while loop for iteration criterion
 while p_a < p_req:
@@ -161,24 +182,58 @@ while p_a < p_req:
     if A <= CS2[-1, 0]:
         B = getB(CS2, A)
     else:
+        # use [-1] to get last element of an array
         B = CS2[-1, 1]
 
     # for now assume Dot >4
     # Calculate allowable pressure
     p_a = (4*B)/(3 * Dt)
 
-    # equivalent to c++'s i++, inc itnum to avoid infinite loop
-    itnum += 1
+    # Add to summary array
+    it.append(itnum)
+    t_arr.append(t)
+    Dt_arr.append(Dt)
+    LD_arr.append(LD)
+    A_arr.append(A)
+    B_arr.append(B)
+    pa_arr.append(p_a)
+
+    # Check if current it >= max
     if itnum >= maxit:
-        print()
-        print('Did not find solution after %i iterations' %itnum)
+        # multiple blank lines
+        print('\n' * 2)
+        print('Did not find solution after %i iterations' % itnum)
         break
     # check if solution converged, print messages and then it will exit loop
     elif p_a >= p_req:
         print('A thickness of %.3f in will be safe' %t)
         print('pa = %.1f psi >= preq = %.1f psi' %(p_a, p_req))
 
-    print()
-        # Array export
-# Q_Exp = np.asarray([x_0, q])
-# np.savetxt('Data\DistLoadCap.csv', np.transpose(Q_Exp), delimiter=",")
+    # equivalent to c++'s i++, inc itnum to avoid infinite loop
+    itnum += 1
+
+    # multiple blank lines
+    print('\n' * 2)
+
+# Do final stuff
+doback = 0
+if doback == 1:
+
+    # Plot D/t mass vs L/D vs A Fig G ASME
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(X_Dt, Y_LD, FigG_A3D, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    # Customize the z axis.
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.0f'))
+    ax.set_xlabel(r'$D_o/t$')
+    ax.set_ylabel(r'$L/D_o$')
+    ax.set_zlabel(r'$A$')
+    plt.title('Visualization of ASME Fig-G')
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.savefig('fig\FigG_3D.png')
+    plt.show()
+
+    # Array export
+    Exp_Data = np.asarray([it, t_arr, Dt_arr, LD_arr, A_arr, B_arr, pa_arr])
+    np.savetxt('data\it_sum.csv', np.transpose(Exp_Data), delimiter=',')
